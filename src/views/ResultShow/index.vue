3ab03d68b60fee34db831a6cfdf51b755bbb0ad3
JinShiyin <shiyinjin@foxmail.com>
2020-12-04 09:35:05 +0800

    初始化结果展示页面

---
+++ src/views/ResultShow/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button> -->
+    <div class="result-show" style="width: 100vw; height: 100vw; display: flex; justify-content: center; align-items: center; background-color: cornflowerblue;">
+      <div class="image" style="width: 80%; height: 80%;">
+        <van-image src="https://s3.ax1x.com/2020/12/03/D7XA9P.jpg" style="width: 100%; height: 100%;">
+        </van-image>
+
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+
